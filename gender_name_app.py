64514a6854e8e44ab08c27b6765f8d450ee43471
David Mo <damoster576@gmail.com>
2018-08-30 22:50:05 +1000

    Added Swagger documentation and API adjustments for SwaggerUI
    API adjustments are for integration with swagger UI, before adjustments, APIs worked fine

--- gender_name_app.py
+++ gender_name_app.py
@@ -1,13 +1,13 @@
 from flask import Flask, request, Response, jsonify
+from flask_cors import CORS
 import ml_module
 
-# TODO make return an http response, i.e. JSON object
-# TODO add data validation for POST requests
 # Useful URLS
 # http://blog.luisrei.com/articles/flaskrest.html
 # https://restfulapi.net/http-status-codes/
 
 app = Flask(__name__)
+CORS(app) # For Swagger UI integration
 gender_name_model = ml_module.gender_name()
 
 def errorResponse(error_msg, status_code):
@@ -19,7 +19,7 @@ def errorResponse(error_msg, status_code):
   resp.status_code = status_code
   return resp
 
-@app.route("/retrain")
+@app.route("/retrain", methods=['POST'])
 def retrain():
   try:
     accuracy = gender_name_model.retrain()
@@ -31,9 +31,9 @@ def retrain():
     return resp
   except Exception as e:
     # print(e)
-    return errorResponse("Please contact server admin",500)
+    return errorResponse("Internal Server Error",500)
 
-@app.route("/reset")
+@app.route("/reset", methods=['POST'])
 def reset():
   try:
     accuracy = gender_name_model.reset()
@@ -45,43 +45,36 @@ def reset():
     return resp
   except Exception as e:
     # print(e)
-    return errorResponse("Please contact server admin",500)
+    return errorResponse("Internal Server Error",500)
 
 @app.route("/predict", methods=['GET'])
 def predict():
   try:
     name = request.args['name']
   except:
-    return errorResponse("Invalid request parameters", 400)
+    return errorResponse("Invalid input", 400)
 
   prediction = gender_name_model.predict(name)
   
   data = {
-    'name':name.title(),
     'prediction':prediction
   }
   resp = jsonify(data)
   resp.status_code = 200
   return resp
 
-# curl -d "name=ray&label=M" -X POST http://127.0.0.1:5000/add
 # curl -d '{"name":"ray","label":"M"}' -H "Content-Type: application/json" -X POST http://127.0.0.1:5000/add
-@app.route("/add", methods=['POST'])
+@app.route("/add", methods=['POST', 'OPTION'])
 def add():
-
   if request.is_json: # Content-Type: application/json
     request_json = request.get_json()
     try:
       name = request_json.get('name')
       label = request_json.get('label')
     except:
-      return errorResponse("Invalid request parameters", 400)
-  elif len(request.form) == 2: # Content-Type:
-    try:
-      name = request.form['name']
-      label = request.form['label']
-    except:
-      return errorResponse("Invalid request parameters", 400)
+      return errorResponse("Invalid input", 400)
+  else:
+    return errorResponse("Invalid input", 400)
 
   is_success = gender_name_model.add(name, label) # TODO, maybe return new total data count?
   data = {
@@ -92,6 +85,4 @@ def add():
   return resp
 
 if __name__ == "__main__":
-  # initialise ML model with current data pool
-  # currently done in global variable for model definition
   app.run()
--- ml_module.py
+++ ml_module.py
@@ -9,9 +9,9 @@ class gender_name:
     self.curr_csv = './data/curr_data.csv'
     self.prop_train = 0.66 # % proportion of data to use for training set
     self.classifier = None
-    #
+
     self.retrain() # Re-build the model on initialisation
-    #
+
   # Relevant gender features of a name
   def gender_features(self, name):
     return {
@@ -21,6 +21,7 @@ class gender_name:
       'suffix3':name[-3:],
       'length':len(name)
     }
+
   def retrain(self):
     # Import data
     labelled_names = []
@@ -37,29 +38,26 @@ class gender_name:
         # else:
         #   print("ERROR"+row[1])
         #   exit()
-    #
+
     random.shuffle(labelled_names)
     names = labelled_names
     num_train = int(self.prop_train * len(names))
     num_test = len(names) - num_train
-    #
+
     featuresets = [(self.gender_features(n), gender) for (n, gender) in names]
     train_set, test_set = featuresets[num_train:], featuresets[:num_test]
     self.classifier = nltk.NaiveBayesClassifier.train(train_set)
-    #
+
     return(nltk.classify.accuracy(self.classifier, test_set))
-    #
+
   def reset(self):
     copyfile(self.init_csv, self.curr_csv)
-    return(self.retrain())    
+    return(self.retrain())
+
   def predict(self, name):
     return self.classifier.classify(self.gender_features(name))
-    #
+    
   def add(self, name, label):
-    # TODO should type check and validation be done in module or in HTTP?
-    # I think should be done in HTTP so we assume we have valid input here
-    # Should the try catches be here or in HTTP? I think in the HTTP
-
     # TODO should probs check for duplicates before adding
     try:
       with open(self.curr_csv,'a') as fd:
